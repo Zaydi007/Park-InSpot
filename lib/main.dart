33132c22de8fe108d2c9c689e65008091111f7ba
trentfowler01 <trentfowler01@gmail.com>
2024-04-16 18:14:20 -0500

    Changed the signup image bacground to a color so we dont need an asset import

--- lib/main.dart
+++ lib/main.dart
@@ -97,8 +97,78 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Login'),
       ),
-      body: Center(
-        child: Text('This is the login page.'),
+      body: Container(
+        color: Color(0xFFA7AEDA),
+        child: Center(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Sign Up',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  DropdownButtonFormField<String>(
+                    value: 'Jellyfish Lot',
+                    onChanged: (String? newValue) {},
+                    items: <String>[
+                      'Jellyfish Lot',
+                      'Turtle Cove Lot',
+                      'Curlew Lot',
+                      'Seahorse Lot',
+                    ].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    decoration: InputDecoration(
+                      labelText: 'Select Parking Lot',
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  DropdownButtonFormField<String>(
+                    value: 'Motorcycle',
+                    onChanged: (String? newValue) {},
+                    items: <String>['Motorcycle', 'Car', 'Disabled']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    decoration: InputDecoration(
+                      labelText: 'Select Vehicle Type',
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Implement sign-up logic
+                    },
+                    child: Text('Sign Up'),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ),
       ),
     );
   }
@@ -112,12 +182,7 @@ class SignUpPage extends StatelessWidget {
         title: Text('Sign Up'),
       ),
       body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/signuppagebg.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
+         color: Color(0xFFA7AEDA),    
         child: Center(
           child: Padding(
             padding: const EdgeInsets.all(20.0),
@@ -139,40 +204,6 @@ class SignUpPage extends StatelessWidget {
                 TextFormField(
                   decoration: InputDecoration(
                     labelText: 'Password',
-                  ),
-                ),
-                SizedBox(height: 10),
-                DropdownButtonFormField<String>(
-                  value: 'Jellyfish Lot',
-                  onChanged: (String? newValue) {},
-                  items: <String>[
-                    'Jellyfish Lot',
-                    'Turtle Cove Lot',
-                    'Curlew Lot',
-                    'Seahorse Lot',
-                  ].map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  decoration: InputDecoration(
-                    labelText: 'Select Parking Lot',
-                  ),
-                ),
-                SizedBox(height: 10),
-                DropdownButtonFormField<String>(
-                  value: 'Motorcycle',
-                  onChanged: (String? newValue) {},
-                  items: <String>['Motorcycle', 'Car', 'Disabled']
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  decoration: InputDecoration(
-                    labelText: 'Select Vehicle Type',
                   ),
                 ),
                 SizedBox(height: 20),
